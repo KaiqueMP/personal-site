60d399c784d59dc9809664e97ae1aa6c27c41a6b
Kaique <artes.kaiquemarques@hotmail.com>
2020-03-13 17:08:02 -0300

    Correção
    correção de espaçamentos

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -6,30 +6,37 @@ import './inicio.css'
 class Inicio extends Component{
     render(){
         return(
-            <div className="container">
-                <div clasName="leftItems">   
-                <Typical
-                    steps={["< Hello i'm />", 3000, '< Olá, meu nome é />', 5000]}
-                    loop={Infinity}
-                    wrapper="p"
-                    className="saudacao"
-                />
-                    <h2> Kaique Marques </h2>
-                    <h3>UX/UI Designer and Front end developer</h3>
-                    <p>Lorem ipsum a massa hendrerit pellentesque purus curabitur 
-                        imperdiet lectus gravida ornare eleifend, aliquet etiam 
-                        neque aliquet class phasellus quisque purus ut ac. vel 
-                        faucibus dui taciti orci nam fusce vestibulum eget, 
-                        fermentum nisi platea justo vestibulum
-                    </p>
+            <div>
+                <div className="container">
+                    <div id="leftItems">   
+                    <Typical
+                        steps={["< Hello i'm />", 3000, '< Olá, meu nome é />', 5000]}
+                        loop={Infinity}
+                        wrapper="p"
+                        className="saudacao"
+                    />
+                        <h2> Kaique Marques </h2>
+                        <h3>UX/UI Designer and Front end developer</h3>
+                        <p>Lorem ipsum a massa hendrerit pellentesque purus curabitur 
+                            imperdiet lectus gravida ornare eleifend, aliquet etiam 
+                            neque aliquet class phasellus quisque purus ut ac. vel 
+                            faucibus dui taciti orci nam fusce vestibulum eget, 
+                            fermentum nisi platea justo vestibulum
+                        </p>
 
-                <button className="buttonSolid" href="">Projetos</button>
-                    <button className="buttonBorder" href="">Contato</button>
+                    <button className="buttonSolid" href="">Projetos</button>
+                        <button className="buttonBorder" href="">Contato</button>
+                    </div>
+
+                    <div className="rightItems">
+                        <img className="illustration" src={require('./assets/Ilustracao-inicio.png')} />
+                    </div>
                 </div>
 
-                <div className="rightItems">
-                    <img className="illustration" src={require('./assets/Ilustracao-inicio.png')} />
+                <div className="scroll">
+                        <img src={require('./assets/scroll.svg')} />
                 </div>
+
             </div>
 
             
