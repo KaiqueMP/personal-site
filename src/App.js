cc5933287d57b7a1b15bc890c0cf163d8df24729
Kaique Marques <artes.kaiquemarques@hotmail.com>
2020-04-23 22:11:17 -0300

    Subindo tela de Em Breve

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Menu from './pages/Header/Menu.js'
-import Inicio from './pages/Inicio/Inicio.js'
-import Sobre from './pages/sobre/sobre';
+// import Menu from './pages/Header/Menu.js'
+// import Inicio from './pages/Inicio/Inicio.js'
+// import Sobre from './pages/sobre/sobre';
+import EmBreve from './pages/EmBreve/EmBreve.js'
 
 
 function App() {
   return (
     <div>
       {/* <Menu /> */}
-      <Inicio />
-      <Sobre />
+      <EmBreve />
+      
     </div>
   );
 }
---
+++ src/pages/EmBreve/EmBreve.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Typical from 'react-typical'
+import './EmBreve.css'
+
+
+class EmBreve extends Component{
+    render(){
+        return(
+            <main className="container-inicio">
+                    <section className="hero container-inicio">   
+                     <Typical
+                            steps={["< Hello i'm />", 3000, '< Olá, meu nome é />', 5000]}
+                            loop={Infinity}
+                            wrapper="p"
+                            className="saudacao"
+                        />
+                        
+                        <h3> Kaique Marques </h3>
+                        <h4>UX/UI Designer and Front end developer</h4>
+                        <p>Ainda estamos em construindo o site, por enquanto me encontre nas redes sociais abaixo ou mande um email para: <br/> <a id="email">contato@kaiquemarques.com.br</a> </p>         
+                        <div className="button">
+                            <a href="https://www.behance.net/KaiqueMarques" target="_blank" rel="noopener noreferrer">
+                                <button className="buttonSolid" ><span className="icon"></span>Projetos</button>
+
+                                <div className="button-icons">
+
+                                    <a id="linkIcon" href="https://www.linkedin.com/in/kaique-marques/" target="_blank">
+                                        <img className="icon"  src={require('../../assets/linkedin-icon.svg')} />
+                                    </a>
+                                    
+                                    <a id="instaIcon" href="https://www.instagram.com/kaiquempo/" target="_blank">
+                                        <img className="icon"  src={require('../../assets/instagram-icon.svg')} />
+                                    </a>
+                                    
+                                    <a href="https://github.com/KaiqueMP" target="_blank">
+                                        <img className="icon"  src={require('../../assets/github-icon.svg')} />
+                                    </a>
+                                </div>
+                            </a>
+
+                            
+                        </div>               
+                    </section>
+
+                
+
+            </main>
+
+            
+        );
+    }
+}
+
+export default EmBreve;
